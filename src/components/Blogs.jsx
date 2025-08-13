29283e0d82ce6192139d262580fcb4b5698a3afb
The-Memin <guillejuma@outlook.es>
2025-08-13 10:52:44 -0600

    Part 5 - Exercise 5.10: Blog List Frontend, step 10

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,20 +1,41 @@
 import Blog from '../components/Blog'
 import CreateBlogForm from './CreateBlogForm'
 import Togglable from './Togglable'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import useBlogs from '../hooks/useBlogs'
+
+const SORT = {
+  NONE: "none",
+  ASC: "asc",
+  DESC: "desc"
+}
+
+function sortByLikes(arr, order) {
+  if(order === SORT.ASC) return [...arr].sort((a,b)=>a.likes-b.likes)
+  if(order === SORT.DESC) return [...arr].sort((a,b)=>b.likes-a.likes)
+  return arr
+}
 
 const Blogs = ({user, onLogOut, setNotification}) => {
     const blogRef = useRef()
+    const [order, setOrder] = useState(SORT.NONE)
+
     const {
       blogs,
       addNewBlog,
       updateLikes
-    } = useBlogs()
+    } = useBlogs(setNotification)
     const addBlog = (newBlog)=>{
       blogRef.current.toggleVisibility()
       addNewBlog(newBlog)
     }
+
+    const sortBlogsByLikes = (orderType) => {
+      setOrder(orderType)
+    }
+
+    const blogsToShow = sortByLikes(blogs, order)
+
     return(
         <div>
             <div>
@@ -24,8 +45,10 @@ const Blogs = ({user, onLogOut, setNotification}) => {
               <CreateBlogForm addBlog={addBlog}/>
             </Togglable>
             <div style={ {marginTop: "1em"} }>
-              {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} setNotification={setNotification} updateLikes={updateLikes}/>
+              <button onClick={()=>sortBlogsByLikes(SORT.DESC)}>sort by most likes</button>
+              <button onClick={()=>sortBlogsByLikes(SORT.ASC)}>sort by few likes</button>
+              {blogsToShow.map(blog =>
+                <Blog key={blog.id} blog={blog} updateLikes={updateLikes}/>
               )}
             </div>
         </div>
